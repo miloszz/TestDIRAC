951aff2099d6f6e22b75202ca7f08bc50fbf3c53
fstagni <fstagni@cern.ch>
2013-04-09 16:35:29 +0200

    Added more complete test

--- Integration/Test_UserJobs.py
+++ Integration/Test_UserJobs.py
@@ -1,3 +1,8 @@
+""" Testing the API and a bit more.
+    It will submit a number of test jobs locally (via runLocal), using the python unittest to assess the results.
+    Can be automatized.
+"""
+
 from DIRAC.Core.Base.Script import parseCommandLine
 parseCommandLine()
 
@@ -15,8 +20,8 @@ gLogger.setLevel( 'DEBUG' )
 cwd = os.path.realpath( '.' )
 
 class UserJobTestCase( unittest.TestCase ):
-  ''' Base class for the UserJob test cases
-  '''
+  """ Base class for the UserJob test cases
+  """
   def setUp( self ):
     cleanTestDir()
 
@@ -25,17 +30,56 @@ class UserJobTestCase( unittest.TestCase ):
 
 
 class HelloWorldSuccess( UserJobTestCase ):
+  """ Simplest ever. helloWorld.py just calls 'echo'
+  """
   def test_execute( self ):
 
     job = Job()
 
     job.setName( "helloWorld-test" )
-    job.setExecutable( "exe-script.py" )
+    job.setExecutable( "helloWorld.py" )
     res = job.runLocal( self.dirac )
     self.assertTrue( res['OK'] )
+
+class HelloWorldPlusSuccess( UserJobTestCase ):
+  """ Adding quite a lot of calls from the API, for pure test purpose
+  """
+
+  def test_execute( self ):
+
+    job = Job()
+
+    job.setName( "helloWorld-test" )
+    job.setExecutable( "helloWorld.py", arguments = "This is an argument", logFile = "aLogFileForTest.txt" )
+    job.setBannedSites( ['LCG.SiteA.com', 'DIRAC.SiteB.org'] )
+    job.setOwner( 'ownerName' )
+    job.setOwnerGroup( 'ownerGroup' )
+    job.setName( 'jobName' )
+    job.setJobGroup( 'jobGroup' )
+    job.setType( 'jobType' )
+    job.setDestination( 'DIRAC.someSite.ch' )
+    job.setCPUTime( 12345 )
+    job.setLogLevel( 'DEBUG' )
+
+    res = job.runLocal( self.dirac )
+    self.assertTrue( res['OK'] )
+
+
+# class CatSuccess( UserJobTestCase ):
+#  def test_execute( self ):
+#
+#    job = Job()
+#
+#    job.setName( "cat-test" )
+#    job.setExecutable( "cat.py" )
+#    job.set
+#    res = job.runLocal( self.dirac )
+#    self.assertTrue( res['OK'] )
+
 
 if __name__ == '__main__':
   suite = unittest.defaultTestLoader.loadTestsFromTestCase( UserJobTestCase )
   suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( HelloWorldSuccess ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( HelloWorldPlusSuccess ) )
   testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
 
