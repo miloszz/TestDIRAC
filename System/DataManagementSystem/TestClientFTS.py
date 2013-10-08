08f86219934fb211385383b7cb4fa512aeaa91b1
fstagni <fstagni@cern.ch>
2013-10-08 18:20:10 +0200

    Fixed FTS test

--- System/DataManagementSystem/TestClientFTS.py
+++ System/DataManagementSystem/TestClientFTS.py
@@ -7,6 +7,7 @@
 import unittest, mock
 import uuid
 
+from DIRAC import gLogger
 from DIRAC.DataManagementSystem.Client.FTSFile import FTSFile
 from DIRAC.DataManagementSystem.Client.FTSJob import FTSJob
 from DIRAC.DataManagementSystem.Client.FTSSite import FTSSite
@@ -24,7 +25,7 @@ class FTSDBTestCase( unittest.TestCase ):
   def setUp( self ):
     """ test case set up """
 
-
+    gLogger.setLevel( 'NOTICE' )
 
     self.ftsSites = [ FTSSite( ftsServer = 'https://fts22-t0-export.cern.ch:8443/glite-data-transfer-fts/services/FileTransfer', name = 'CERN.ch' ),
                       FTSSite( ftsServer = 'https://fts.pic.es:8443/glite-data-transfer-fts/services/FileTransfer', name = 'PIC.es' ),
@@ -56,6 +57,7 @@ class FTSDBTestCase( unittest.TestCase ):
         ftsJob.Completeness = 90
       ftsJob.SourceSE = self.ses[ i % len( self.ses ) ]
       ftsJob.TargetSE = 'PIC-USER'
+      ftsJob.RequestID = 12345
 
       ftsFile = FTSFile()
       ftsFile.FileID = i + 1
@@ -83,7 +85,19 @@ class FTSDBTestCase( unittest.TestCase ):
     self.submitted = len( [ i for i in self.ftsJobs if i.Status == 'Submitted' ] )
 
     self.ftsClient = FTSClient()
-    self.ftsClient.ftsManager.replicaManager = mock.Mock()
+    self.ftsClient.replicaManager = mock.Mock()
+    self.ftsClient.replicaManager.getActiveReplicas.return_value = {'OK': True,
+                                                                    'Value': {'Successful': {'/a/b/c/1':{'CERN-USER':'/aa/a/b/c/1d',
+                                                                                                         'RAL-USER':'/bb/a/b/c/1d'},
+                                                                                             '/a/b/c/2':{'CERN-USER':'/aa/a/b/c/2d',
+                                                                                                         'RAL-USER':'/bb/a/b/c/2d'},
+                                                                                             '/a/b/c/3':{'CERN-USER':'/aa/a/b/c/3d',
+                                                                                                         'RAL-USER':'/bb/a/b/c/3d'}
+                                                                                             },
+                                                                              'Failed': {'/a/b/c/4':'/aa/a/b/c/4d',
+                                                                                         '/a/b/c/5':'/aa/a/b/c/5d'}
+                                                                              }
+                                                                    }
 
   def tearDown( self ):
     """ clean up """
@@ -157,33 +171,49 @@ class FTSClientMix( FTSDBTestCase ):
 
   def test_mix( self ):
     """ all the other tests"""
+    opFileList = []
     for ftsJob in self.ftsJobs:
-      print self.ftsClient.putFTSJob( ftsJob )
+      self.ftsClient.putFTSJob( ftsJob )
+      opFileList.append( ( ftsJob[0].toJSON()["Value"], self.ses, self.ses ) )
 
-
-#    ftsSchedule
-
-#    print 'setFTSFilesWaiting'
-#    for operationID in self.opIDs:
-#      for sourceSE in self.ses:
-#        res = self.ftsClient.setFTSFilesWaiting( operationID, sourceSE )
-#        self.assertEqual( res['OK'], True )
-
-#    getFTSHistory
-#    getFTSJobsForRequest
-#    getFTSFilesForRequest
-#    getDBSummary
-
-#    print 'deleteJob'
-#    for i in FTSjobIDs:
-#      delete = self.ftsClient.deleteFTSJob( i )
-#      self.assertEqual( delete['OK'], True )
-#
-#    print 'deleteFiles'
-#    for i in self.opIDs:
-#      res = self.ftsClient.deleteFTSFiles( i )
+#    ftsSchedule can't work since the FTSStrategy object is refreshed in the service so it can't be mocked
+#    for opID in self.opIDs:
+#      res = self.ftsClient.ftsSchedule( 12345, opID, opFileList )
 #      self.assert_( res['OK'] )
 
+    print 'setFTSFilesWaiting'
+    for operationID in self.opIDs:
+      for sourceSE in self.ses:
+        res = self.ftsClient.setFTSFilesWaiting( operationID, sourceSE )
+        self.assertEqual( res['OK'], True )
+
+    print 'getFTSHistory'
+    res = self.ftsClient.getFTSHistory()
+    self.assertEqual( res['OK'], True )
+    self.assert_( type( res['Value'] ) == type( [] ) )
+
+    print 'getFTSJobsForRequest'
+    res = self.ftsClient.getFTSJobsForRequest( 12345 )
+    self.assertEqual( res['OK'], True )
+
+    print 'getFTSFilesForRequest'
+    res = self.ftsClient.getFTSFilesForRequest( 12345 )
+    self.assertEqual( res['OK'], True )
+
+    print 'getDBSummary'
+    res = self.ftsClient.getDBSummary()
+    self.assertEqual( res['OK'], True )
+
+    FTSjobIDs = self.ftsClient.getFTSJobIDs( self.statuses )['Value']
+    print 'deleteJob'
+    for i in FTSjobIDs:
+      delete = self.ftsClient.deleteFTSJob( i )
+      self.assertEqual( delete['OK'], True )
+
+    print 'deleteFiles'
+    for i in self.opIDs:
+      res = self.ftsClient.deleteFTSFiles( i )
+      self.assert_( res['OK'] )
 
 
 if __name__ == '__main__':
